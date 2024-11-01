0308a7de81fad227b9711f757e088cba70400909
jose leonel <90006477+joseleonelmacedo@users.noreply.github.com>
2024-11-01 00:07:58 -0300

    ta feito minha patroa

--- calculoMedia.js
+++ calculoMedia.js
@@ -1,13 +1,14 @@
 function calcularMedia(up1, up2) {
-    media = up1 + up2 / 2;
-    if (media > 6){
+    let media = (up1 + up2) / 2;
+    if (media >= 6) {
         return "Aprovado por média";
     } 
-    if ( media < 4) {
+    if (media < 4) {
         return "Reprovado por média";
-    }
-    else{
-        final = 12 - media;
-        return `Aguardando a final nota necessaria sera ${final}`;
+    } else {
+        let final = 12 - media;
+        return `Aguardando a final, nota necessária será ${final.toFixed(2)}`;
     }
 }
+
+module.exports = { calcularMedia };
