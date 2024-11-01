e926a00813bb8141ed90f1ba65dbd18be3fe2d9b
jose leonel <90006477+joseleonelmacedo@users.noreply.github.com>
2024-10-31 23:32:27 -0300

    calcularMedia

--- calculoMedia.js
+++ calculoMedia.js
@@ -1,3 +1,13 @@
 function calcularMedia(up1, up2) {
-    if 
+    media = up1 + up2 / 2;
+    if (media > 6){
+        return "Aprovado por média";
+    } 
+    if ( media < 4) {
+        return "Reprovado por média";
+    }
+    else{
+        final = 12 - media;
+        return `Aguardando a final nota necessaria sera ${final}`;
+    }
 }
